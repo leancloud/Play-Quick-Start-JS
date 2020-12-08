8e7d049ec4217d010396386a2cb80f1c3205ff4d
oneRain <onerain88@gmail.com>
2020-12-08 10:12:52 +0800

    chore: cocos

--- CocosCreator/assets/Script/HelloWorld.js
+++ CocosCreator/assets/Script/HelloWorld.js
@@ -6,7 +6,14 @@ if (cc.sys.WECHAT_GAME == cc.sys.platform) {
   PlaySDK = Play;
 }
 
-const { Client, Event, ReceiverGroup, setAdapters, LogLevel, setLogger } = PlaySDK;
+const {
+  Client,
+  Event,
+  ReceiverGroup,
+  setAdapters,
+  LogLevel,
+  setLogger
+} = PlaySDK;
 
 const GAME_OVER_EVENT = 100;
 
@@ -36,7 +43,7 @@ cc.Class({
     if (cc.sys.platform === cc.sys.ANDROID) {
       const caPath = cc.url.raw("resources/cacert.pem");
       setAdapters({
-        WebSocket: url => new WebSocket(url, 'protobuf.1', caPath)
+        WebSocket: url => new WebSocket(url, "protobuf.1", caPath)
       });
     }
 
@@ -44,73 +51,70 @@ cc.Class({
       [LogLevel.Debug]: console.log.bind(console)
     });
 
-    
     const play = new Client({
-      appId: 'g2b0X6OmlNy7e4QqVERbgRJR-gzGzoHsz',
-      appKey: 'CM91rNV8cPVHKraoFQaopMVT',
+      appId: "g2b0X6OmlNy7e4QqVERbgRJR-gzGzoHsz",
+      appKey: "CM91rNV8cPVHKraoFQaopMVT",
       userId: randId.toString(),
-      playServer: 'https://g2b0x6om.lc-cn-n1-shared.com',
+      playServer: "https://g2b0x6om.lc-cn-n1-shared.com"
     });
 
-    play.connect()
-      .then(() => {
-        const now = new Date();
-        const hour = now.getHours();
-        const minute = now.getMinutes();
-        const roomName = `${hour}_${minute}`;
-        this.idLabel.string = roomName;
-        play.joinOrCreateRoom(roomName)
-          .then(() => {
-            play.on(Event.PLAYER_ROOM_JOINED, data => {
-              const { newPlayer } = data;
-              console.log(`new player: ${newPlayer.userId}`);
-              if (play.player.isMaster) {
-                // 获取房间玩家列表
-                const playerList = play.room.playerList;
-                for (let i = 0; i < playerList.length; i++) {
-                  const player = playerList[i];
-                  // 判断如果是房主，则设置 10 分，否则设置 5 分
-                  if (player.isMaster) {
-                    player.setCustomProperties({
-                      point: 10
-                    });
-                  } else {
-                    player.setCustomProperties({
-                      point: 5
-                    });
-                  }
-                }
-                play.sendEvent(
-                  GAME_OVER_EVENT,
-                  { winnerId: play.room.masterId },
-                  { receiverGroup: ReceiverGroup.All }
-                );
+    play.connect().then(() => {
+      const now = new Date();
+      const hour = now.getHours();
+      const minute = now.getMinutes();
+      const roomName = `${hour}_${minute}`;
+      this.idLabel.string = roomName;
+      play.joinOrCreateRoom(roomName).then(() => {
+        play.on(Event.PLAYER_ROOM_JOINED, data => {
+          const { newPlayer } = data;
+          console.log(`new player: ${newPlayer.userId}`);
+          if (play.player.isMaster) {
+            // 获取房间玩家列表
+            const playerList = play.room.playerList;
+            for (let i = 0; i < playerList.length; i++) {
+              const player = playerList[i];
+              // 判断如果是房主，则设置 10 分，否则设置 5 分
+              if (player.isMaster) {
+                player.setCustomProperties({
+                  point: 10
+                });
+              } else {
+                player.setCustomProperties({
+                  point: 5
+                });
               }
-            });
-            play.on(Event.PLAYER_CUSTOM_PROPERTIES_CHANGED, data => {
-              const { player } = data;
-              const { point } = player.customProperties;
-              console.log(`${player.userId}: ${point}`);
-              if (player.isLocal) {
-                this.scoreLabel.string = `score:${point}`;
-              }
-            });
-            play.on(Event.CUSTOM_EVENT, event => {
-              // 解构事件参数
-              const { eventId, eventData } = event;
-              if (eventId === GAME_OVER_EVENT) {
-                const { winnerId } = eventData;
-                console.log(`winnerId: ${winnerId}`);
-                // 如果胜利者是自己，则显示胜利 UI；否则显示失败 UI
-                if (play.player.actorId === winnerId) {
-                  this.resultLabel.string = "win";
-                } else {
-                  this.resultLabel.string = "lose";
-                }
-                play.close();
-              }
-            });
-          });
-      })
+            }
+            play.sendEvent(
+              GAME_OVER_EVENT,
+              { winnerId: play.room.masterId },
+              { receiverGroup: ReceiverGroup.All }
+            );
+          }
+        });
+        play.on(Event.PLAYER_CUSTOM_PROPERTIES_CHANGED, data => {
+          const { player } = data;
+          const { point } = player.customProperties;
+          console.log(`${player.userId}: ${point}`);
+          if (player.isLocal) {
+            this.scoreLabel.string = `score:${point}`;
+          }
+        });
+        play.on(Event.CUSTOM_EVENT, event => {
+          // 解构事件参数
+          const { eventId, eventData } = event;
+          if (eventId === GAME_OVER_EVENT) {
+            const { winnerId } = eventData;
+            console.log(`winnerId: ${winnerId}`);
+            // 如果胜利者是自己，则显示胜利 UI；否则显示失败 UI
+            if (play.player.actorId === winnerId) {
+              this.resultLabel.string = "win";
+            } else {
+              this.resultLabel.string = "lose";
+            }
+            play.close();
+          }
+        });
+      });
+    });
   }
 });
--- CocosCreator/assets/Script/play.js
+++ CocosCreator/assets/Script/play.js
@@ -5958,7 +5958,7 @@
 
 	    } }]);return GameRouter;}();
 
-	var version = "0.18.0-beta.7";
+	var version = "0.18.0-beta.10";
 	var protocolVersion = "0";
 
 	// SDK 版本号
@@ -22100,12 +22100,13 @@
 	  LOBBY: 5,
 	  STATISTIC: 6,
 	  EVENTS: 7,
-	  ACK: 8 };
+	  ACK: 8,
+	  ECHO: 9 };
 
 
 	/**
-	             * @enum {number}
-	             */
+	              * @enum {number}
+	              */
 	proto.game_protobuf_messages.proto.messages.OpType = {
 	  NONE: 0,
 	  OPEN: 1,
@@ -22257,6 +22258,7 @@
 	      // 每次连接成功后将会得到最新快照，之前的缓存没有意义了
 	      this._isMessageQueueRunning = true;
 	      this._messageQueue = [];
+	      this._ping();
 	      this._ws.onmessage = function (message) {
 	        _this3._pong();
 	        var command = Command.deserializeBinary(message.data);
@@ -22344,8 +22346,6 @@
 	      _JSON$stringify(body.toObject()));
 
 	      this._ws.send(command.serializeBinary().buffer);
-	      // ping
-	      this._ping();
 	    } }, { key: 'close', value: function close()
 
 	    {var _this5 = this;
@@ -22392,8 +22392,9 @@
 	        clearTimeout(this._pingTimer);
 	        this._pingTimer = null;
 	      }
-	      this._pingTimer = setTimeout(function () {
-	        _this6._ws.send('{}');
+	      this._pingTimer = setInterval(function () {
+	        debug('ping');
+	        _this6.sendCommand(CommandType.ECHO, OpType.NONE, new Body());
 	      }, this._getPingDuration());
 	    } }, { key: '_pong', value: function _pong()
 
@@ -22403,9 +22404,8 @@
 	        this._pongTimer = null;
 	      }
 	      this._pongTimer = setTimeout(function () {
-	        _this7._pingTimer = setTimeout(function () {
-	          _this7._ws.close();
-	        }, _this7._getPingDuration());
+	        debug('pong timeout');
+	        _this7._ws.close();
 	      }, this._getPingDuration() * MAX_NO_PONG_TIMES);
 	    } }, { key: '_stopKeppAlive', value: function _stopKeppAlive()
 
@@ -22429,8 +22429,8 @@
 	      throw new Error('must implement the method');
 	    } }, { key: '_handleErrorNotify', value: function _handleErrorNotify(
 
-	    body) {
-	      var errorInfo = body.getErrorInfo();
+	    err) {
+	      var errorInfo = err.getErrorInfo();
 	      var code = errorInfo.getRaseonCode();
 	      var detail = errorInfo.getDetail();
 	      this.emit(ERROR_EVENT, { code: code, detail: detail });
@@ -22524,6 +22524,428 @@
 	});
 
 	var _get = unwrapExports(get);
+
+	var strictUriEncode = str => encodeURIComponent(str).replace(/[!'()*]/g, x => `%${x.charCodeAt(0).toString(16).toUpperCase()}`);
+
+	var token = '%[a-f0-9]{2}';
+	var singleMatcher = new RegExp(token, 'gi');
+	var multiMatcher = new RegExp('(' + token + ')+', 'gi');
+
+	function decodeComponents(components, split) {
+		try {
+			// Try to decode the entire string first
+			return decodeURIComponent(components.join(''));
+		} catch (err) {
+			// Do nothing
+		}
+
+		if (components.length === 1) {
+			return components;
+		}
+
+		split = split || 1;
+
+		// Split the array in 2 parts
+		var left = components.slice(0, split);
+		var right = components.slice(split);
+
+		return Array.prototype.concat.call([], decodeComponents(left), decodeComponents(right));
+	}
+
+	function decode(input) {
+		try {
+			return decodeURIComponent(input);
+		} catch (err) {
+			var tokens = input.match(singleMatcher);
+
+			for (var i = 1; i < tokens.length; i++) {
+				input = decodeComponents(tokens, i).join('');
+
+				tokens = input.match(singleMatcher);
+			}
+
+			return input;
+		}
+	}
+
+	function customDecodeURIComponent(input) {
+		// Keep track of all the replacements and prefill the map with the `BOM`
+		var replaceMap = {
+			'%FE%FF': '\uFFFD\uFFFD',
+			'%FF%FE': '\uFFFD\uFFFD'
+		};
+
+		var match = multiMatcher.exec(input);
+		while (match) {
+			try {
+				// Decode as big chunks as possible
+				replaceMap[match[0]] = decodeURIComponent(match[0]);
+			} catch (err) {
+				var result = decode(match[0]);
+
+				if (result !== match[0]) {
+					replaceMap[match[0]] = result;
+				}
+			}
+
+			match = multiMatcher.exec(input);
+		}
+
+		// Add `%C2` at the end of the map to make sure it does not replace the combinator before everything else
+		replaceMap['%C2'] = '\uFFFD';
+
+		var entries = Object.keys(replaceMap);
+
+		for (var i = 0; i < entries.length; i++) {
+			// Replace all decoded components
+			var key = entries[i];
+			input = input.replace(new RegExp(key, 'g'), replaceMap[key]);
+		}
+
+		return input;
+	}
+
+	var decodeUriComponent = function (encodedURI) {
+		if (typeof encodedURI !== 'string') {
+			throw new TypeError('Expected `encodedURI` to be of type `string`, got `' + typeof encodedURI + '`');
+		}
+
+		try {
+			encodedURI = encodedURI.replace(/\+/g, ' ');
+
+			// Try the built in decoder first
+			return decodeURIComponent(encodedURI);
+		} catch (err) {
+			// Fallback to a more advanced decoder
+			return customDecodeURIComponent(encodedURI);
+		}
+	};
+
+	var splitOnFirst = (string, separator) => {
+		if (!(typeof string === 'string' && typeof separator === 'string')) {
+			throw new TypeError('Expected the arguments to be of type `string`');
+		}
+
+		if (separator === '') {
+			return [string];
+		}
+
+		const separatorIndex = string.indexOf(separator);
+
+		if (separatorIndex === -1) {
+			return [string];
+		}
+
+		return [
+			string.slice(0, separatorIndex),
+			string.slice(separatorIndex + separator.length)
+		];
+	};
+
+	function encoderForArrayFormat(options) {
+		switch (options.arrayFormat) {
+			case 'index':
+				return key => (result, value) => {
+					const index = result.length;
+					if (value === undefined || (options.skipNull && value === null)) {
+						return result;
+					}
+
+					if (value === null) {
+						return [...result, [encode(key, options), '[', index, ']'].join('')];
+					}
+
+					return [
+						...result,
+						[encode(key, options), '[', encode(index, options), ']=', encode(value, options)].join('')
+					];
+				};
+
+			case 'bracket':
+				return key => (result, value) => {
+					if (value === undefined || (options.skipNull && value === null)) {
+						return result;
+					}
+
+					if (value === null) {
+						return [...result, [encode(key, options), '[]'].join('')];
+					}
+
+					return [...result, [encode(key, options), '[]=', encode(value, options)].join('')];
+				};
+
+			case 'comma':
+				return key => (result, value) => {
+					if (value === null || value === undefined || value.length === 0) {
+						return result;
+					}
+
+					if (result.length === 0) {
+						return [[encode(key, options), '=', encode(value, options)].join('')];
+					}
+
+					return [[result, encode(value, options)].join(',')];
+				};
+
+			default:
+				return key => (result, value) => {
+					if (value === undefined || (options.skipNull && value === null)) {
+						return result;
+					}
+
+					if (value === null) {
+						return [...result, encode(key, options)];
+					}
+
+					return [...result, [encode(key, options), '=', encode(value, options)].join('')];
+				};
+		}
+	}
+
+	function parserForArrayFormat(options) {
+		let result;
+
+		switch (options.arrayFormat) {
+			case 'index':
+				return (key, value, accumulator) => {
+					result = /\[(\d*)\]$/.exec(key);
+
+					key = key.replace(/\[\d*\]$/, '');
+
+					if (!result) {
+						accumulator[key] = value;
+						return;
+					}
+
+					if (accumulator[key] === undefined) {
+						accumulator[key] = {};
+					}
+
+					accumulator[key][result[1]] = value;
+				};
+
+			case 'bracket':
+				return (key, value, accumulator) => {
+					result = /(\[\])$/.exec(key);
+					key = key.replace(/\[\]$/, '');
+
+					if (!result) {
+						accumulator[key] = value;
+						return;
+					}
+
+					if (accumulator[key] === undefined) {
+						accumulator[key] = [value];
+						return;
+					}
+
+					accumulator[key] = [].concat(accumulator[key], value);
+				};
+
+			case 'comma':
+				return (key, value, accumulator) => {
+					const isArray = typeof value === 'string' && value.split('').indexOf(',') > -1;
+					const newValue = isArray ? value.split(',') : value;
+					accumulator[key] = newValue;
+				};
+
+			default:
+				return (key, value, accumulator) => {
+					if (accumulator[key] === undefined) {
+						accumulator[key] = value;
+						return;
+					}
+
+					accumulator[key] = [].concat(accumulator[key], value);
+				};
+		}
+	}
+
+	function encode(value, options) {
+		if (options.encode) {
+			return options.strict ? strictUriEncode(value) : encodeURIComponent(value);
+		}
+
+		return value;
+	}
+
+	function decode$1(value, options) {
+		if (options.decode) {
+			return decodeUriComponent(value);
+		}
+
+		return value;
+	}
+
+	function keysSorter(input) {
+		if (Array.isArray(input)) {
+			return input.sort();
+		}
+
+		if (typeof input === 'object') {
+			return keysSorter(Object.keys(input))
+				.sort((a, b) => Number(a) - Number(b))
+				.map(key => input[key]);
+		}
+
+		return input;
+	}
+
+	function removeHash(input) {
+		const hashStart = input.indexOf('#');
+		if (hashStart !== -1) {
+			input = input.slice(0, hashStart);
+		}
+
+		return input;
+	}
+
+	function extract(input) {
+		input = removeHash(input);
+		const queryStart = input.indexOf('?');
+		if (queryStart === -1) {
+			return '';
+		}
+
+		return input.slice(queryStart + 1);
+	}
+
+	function parseValue(value, options) {
+		if (options.parseNumbers && !Number.isNaN(Number(value)) && (typeof value === 'string' && value.trim() !== '')) {
+			value = Number(value);
+		} else if (options.parseBooleans && value !== null && (value.toLowerCase() === 'true' || value.toLowerCase() === 'false')) {
+			value = value.toLowerCase() === 'true';
+		}
+
+		return value;
+	}
+
+	function parse$1(input, options) {
+		options = Object.assign({
+			decode: true,
+			sort: true,
+			arrayFormat: 'none',
+			parseNumbers: false,
+			parseBooleans: false
+		}, options);
+
+		const formatter = parserForArrayFormat(options);
+
+		// Create an object with no prototype
+		const ret = Object.create(null);
+
+		if (typeof input !== 'string') {
+			return ret;
+		}
+
+		input = input.trim().replace(/^[?#&]/, '');
+
+		if (!input) {
+			return ret;
+		}
+
+		for (const param of input.split('&')) {
+			let [key, value] = splitOnFirst(options.decode ? param.replace(/\+/g, ' ') : param, '=');
+
+			// Missing `=` should be `null`:
+			// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+			value = value === undefined ? null : decode$1(value, options);
+			formatter(decode$1(key, options), value, ret);
+		}
+
+		for (const key of Object.keys(ret)) {
+			const value = ret[key];
+			if (typeof value === 'object' && value !== null) {
+				for (const k of Object.keys(value)) {
+					value[k] = parseValue(value[k], options);
+				}
+			} else {
+				ret[key] = parseValue(value, options);
+			}
+		}
+
+		if (options.sort === false) {
+			return ret;
+		}
+
+		return (options.sort === true ? Object.keys(ret).sort() : Object.keys(ret).sort(options.sort)).reduce((result, key) => {
+			const value = ret[key];
+			if (Boolean(value) && typeof value === 'object' && !Array.isArray(value)) {
+				// Sort object keys, not values
+				result[key] = keysSorter(value);
+			} else {
+				result[key] = value;
+			}
+
+			return result;
+		}, Object.create(null));
+	}
+
+	var extract_1 = extract;
+	var parse_1 = parse$1;
+
+	var stringify$2 = (object, options) => {
+		if (!object) {
+			return '';
+		}
+
+		options = Object.assign({
+			encode: true,
+			strict: true,
+			arrayFormat: 'none'
+		}, options);
+
+		const formatter = encoderForArrayFormat(options);
+
+		const objectCopy = Object.assign({}, object);
+		if (options.skipNull) {
+			for (const key of Object.keys(objectCopy)) {
+				if (objectCopy[key] === undefined || objectCopy[key] === null) {
+					delete objectCopy[key];
+				}
+			}
+		}
+
+		const keys = Object.keys(objectCopy);
+
+		if (options.sort !== false) {
+			keys.sort(options.sort);
+		}
+
+		return keys.map(key => {
+			const value = object[key];
+
+			if (value === undefined) {
+				return '';
+			}
+
+			if (value === null) {
+				return encode(key, options);
+			}
+
+			if (Array.isArray(value)) {
+				return value
+					.reduce(formatter(key), [])
+					.join('&');
+			}
+
+			return encode(key, options) + '=' + encode(value, options);
+		}).filter(x => x.length > 0).join('&');
+	};
+
+	var parseUrl = (input, options) => {
+		return {
+			url: removeHash(input).split('?')[0] || '',
+			query: parse$1(extract(input), options)
+		};
+	};
+
+	var queryString = {
+		extract: extract_1,
+		parse: parse_1,
+		stringify: stringify$2,
+		parseUrl: parseUrl
+	};
 
 	var
 
@@ -22841,7 +23263,17 @@
 	    } }, { key: '_getFastOpenUrl', value: function _getFastOpenUrl(
 
 	    server, appId, gameVersion, userId, sessionToken) {
-	      return server + 'session?appId=' + appId + '&sdkVersion=' + version + '&protocolVersion=' + protocolVersion + '&gameVersion=' + gameVersion + '&userId=' + userId + '&sessionToken=' + sessionToken;
+	      var parsedUrl = queryString.parseUrl(server);var
+	      url = parsedUrl.url,query = parsedUrl.query;
+	      var queries = _Object$assign(query, {
+	        appId: appId,
+	        sdkVersion: version,
+	        protocolVersion: protocolVersion,
+	        gameVersion: gameVersion,
+	        userId: userId,
+	        sessionToken: sessionToken });
+
+	      return url + 'session?' + queryString.stringify(queries);
 	    } }, { key: '_handleNotification', value: function _handleNotification(
 
 	    cmd, op, body) {
@@ -22896,7 +23328,7 @@
 	          this._handleSendEventMsg(body.getDirect());
 	          break;
 	        case CommandType$1.ERROR:
-	          _get(GameConnection.prototype.__proto__ || _Object$getPrototypeOf(GameConnection.prototype), '_handleErrorNotify', this).call(this, body);
+	          _get(GameConnection.prototype.__proto__ || _Object$getPrototypeOf(GameConnection.prototype), '_handleErrorNotify', this).call(this, body.getError());
 	          break;
 	        default:
 	          // super._handleUnknownMsg(msg);
@@ -24159,7 +24591,7 @@
 	          // 目前不作处理
 	          break;
 	        case CommandType$2.ERROR:
-	          _get(LobbyConnection.prototype.__proto__ || _Object$getPrototypeOf(LobbyConnection.prototype), '_handleErrorNotify', this).call(this, body);
+	          _get(LobbyConnection.prototype.__proto__ || _Object$getPrototypeOf(LobbyConnection.prototype), '_handleErrorNotify', this).call(this, body.getError());
 	          break;
 	        default:
 	          _get(LobbyConnection.prototype.__proto__ || _Object$getPrototypeOf(LobbyConnection.prototype), '_handleUnknownMsg', this).call(this, cmd, op, body);
